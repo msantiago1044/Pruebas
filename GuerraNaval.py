7f98657d4c91a140fea9637787c27548e4280e0b
Marcelo Barraza <97839381+msantiago1044@users.noreply.github.com>
2025-01-07 11:30:57 -0500

    5

---
+++ GuerraNaval.py
@@ -0,0 +1,40 @@
+import numpy as np
+mar = np.array ([
+            [False,False,False,False,False],
+            [False,False,False,False,False],
+            [False,False,False,False,False],
+            [False,False,False,False,False],
+            [False,False,False,False,False]])
+
+class Barco:
+    def __init__(self, name):
+        self.name = name
+        self.ataques = 2
+
+    def posicion(self, pos1, pos2):
+        pos1=[]
+        pos2=[]
+    
+
+class Jugador:
+    def __init__(self, name):
+        self.name= name
+
+    def atacar(self, ataque):
+        self.ataque= ataque
+        ataque = []
+
+    def colocar_barcos(self, pos1, pos2):
+        Barco.posicion(self, pos1, pos2)
+
+class Plataforma:
+    def __init__(self):
+        self.jugadores=[]
+        self.barcos=[]
+
+    def mostrar_mar():
+        for false in mar:
+        print("Mar")
+
+    def enviar_ataques():
+
